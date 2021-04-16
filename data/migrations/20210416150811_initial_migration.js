4fb33e261c1d1ba9e93b862812d4b743028e3d08
Nicholas Smith <nicholassmithbus@gmail.com>
2021-04-16 09:26:33 -0600

    wrote initial migration

---
+++ data/migrations/20210416150811_initial_migration.js
@@ -0,0 +1,52 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments("project_id");
+      tbl.text("project_name").notNullable();
+      tbl.text("project_description");
+      tbl.boolean("project_completed").defaultTo(0);
+    })
+    .createTable("resources", (tbl) => {
+      tbl.increments("resource_id");
+      tbl.text("resource_name").unique().notNullable();
+      tbl.text("resource_description");
+    })
+    .createTable("tasks", (tbl) => {
+      tbl.increments("task_id");
+      tbl.text("task_description").notNullable();
+      tbl.text("task_notes");
+      tbl.boolean("task_completed").defaultTo(0);
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE");
+    })
+    .createTable("resource_assignments", (tbl) => {
+      tbl.increments("resource_assignment_id");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resource_id")
+        .inTable("resources")
+        .onDelete("CASCADE");
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("resource_assignments")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resource")
+    .dropTableIfExists("project");
+};
